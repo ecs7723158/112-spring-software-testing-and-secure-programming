04cb87a09509b9b261ce17c5ae48f9f94c3e1494
ecs7723158 <ecs7723158@gmail.com>
2024-03-19 23:23:29 +0800

    Update main_test.js

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -39,26 +39,3 @@ test("Test Student's getName", () => {
     
     assert.strictEqual(name, 'Alice');
 });
-const test = require('node:test');
-const assert = require('assert');
-const { MyClass, Student } = require('./main');
-
-test("Test MyClass's addStudent", () => {
-    // TODO
-    throw new Error("Test not implemented");
-});
-
-test("Test MyClass's getStudentById", () => {
-    // TODO
-    throw new Error("Test not implemented");
-});
-
-test("Test Student's setName", () => {
-    // TODO
-    throw new Error("Test not implemented");
-});
-
-test("Test Student's getName", () => {
-    // TODO
-    throw new Error("Test not implemented");
-});
