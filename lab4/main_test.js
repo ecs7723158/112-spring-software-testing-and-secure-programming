0921278f2977733661494f55e16738e0e35e36d8
ecs7723158 <ecs7723158@gmail.com>
2024-05-15 20:55:04 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -7,7 +7,20 @@ const puppeteer = require('puppeteer');
 
     // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
-
+    await wait(100);
+    await page.click('.DocSearch-Button');
+    await wait(100);
+    await page.waitForSelector('#docsearch-input');
+    await wait(100);
+    await page.type('#docsearch-input', 'chipi chipi chapa chapa');
+    await wait(100);
+    await page.waitForSelector('#docsearch-item-5 a');
+    await wait(1000);
+    await page.click('#docsearch-item-5');
+    await page.waitForSelector('h1');
+    await wait(100);
+    const title = await page.evaluate(() => document.querySelector('h1').textContent);
+    console.log(title);
     // Hints:
     // Click search button
     // Type into search box
