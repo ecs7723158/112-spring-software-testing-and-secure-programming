27ae05709be81b2bf4fe775fbcbef02759c6cd80
ecs7723158 <ecs7723158@gmail.com>
2024-05-15 21:02:19 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -7,12 +7,12 @@ const puppeteer = require('puppeteer');
 
     // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
-    await wait(100);
+    //await wait(100);
     await page.click('.DocSearch-Button');
-    await wait(100);
+    //await wait(100);
     await page.waitForSelector('#docsearch-input');
     await page.type('#docsearch-input', 'chipi chipi chapa chapa');
-    await wait(100);
+    //await wait(100);
     await page.waitForSelector('#docsearch-item-5 a');
     await page.click('#docsearch-item-5');
     await page.waitForSelector('h1');
