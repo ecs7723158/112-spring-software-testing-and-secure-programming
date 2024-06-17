1faff1ca65c97bd85acc58146d278bc43a95d8d3
ecs7723158 <ecs7723158@gmail.com>
2024-06-17 09:11:18 +0800

    Update antiasan.c

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,5 +1,9 @@
 // TODO:
 void antiasan(unsigned long addr)
 {
+#include <sanitizer/asan_interface.h>
 
+void antiasan(unsigned long address) {
+
+    __asan_unpoison_memory_region((void *)address, 1);
 }
